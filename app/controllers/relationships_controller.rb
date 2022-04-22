74eb5beee14433c9d746347d8828ae4f44758491
KanaKarimata <kana.lt22@gmail.com>
2022-04-22 12:46:00 +0000

    [Add]follow function

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,23 @@
+class RelationshipsController < ApplicationController
+
+ def create
+   current_user.follow(params[:user_id])
+   redirect_to request.referer
+ end
+ 
+ def destroy
+   current_user.unfollow(params[:user_id])
+   redirect_to request.referer
+ end
+ 
+ def follows
+   user = User.find(params[:id])
+   @users = user.following_user.page(params[:page]).per(3).reverse_order
+ end
+ 
+ def followers
+   user = User.find(params[:id])
+   @users = user.follower_user.page(params[:page]).per(3).reverse_order
+ end
+ 
+end
